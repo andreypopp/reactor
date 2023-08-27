b8cc8cd13624f673a6ded269947770d39faf0a18
Andrey Popp <8mayday@gmail.com>
2023-08-27 19:49:12 +0400

    fmt

--- env_browser/env.ml
+++ env_browser/env.ml
@@ -1,5 +1,6 @@
 module Promise = struct
   type 'a t = 'a Js.Promise.t
+
   let return v = Js.Promise.resolve v
   let ( let* ) v e = Js.Promise.then_ e v
 
@@ -8,6 +9,6 @@ module Promise = struct
     let unit = () in
     ignore
       (Js.Global.setIntervalFloat
-        (fun () -> (resolve unit [@bs]))
-        (sec *. 1000.))
+         (fun () -> (resolve unit [@bs]))
+         (sec *. 1000.))
 end
