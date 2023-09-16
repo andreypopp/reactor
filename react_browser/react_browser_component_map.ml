bfb28ccd7d8b55d7b85484816e8253728491d522
Andrey Popp <8mayday@gmail.com>
2023-09-16 15:31:59 +0400

    Remove submodules as melange 2 is out

--- react_browser/react_browser_component_map.ml
+++ react_browser/react_browser_component_map.ml
@@ -3,7 +3,7 @@ type t = exported_component Js.Dict.t
 
 let t = Js.Dict.empty ()
 
-external window : t Js.Dict.t = "window" [@@bs.val]
+external window : t Js.Dict.t = "window"
 
 let () = Js.Dict.set window "__exported_components" t
 let register name render = Js.Dict.set t name render
