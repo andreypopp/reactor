69e03e33b9eaad5d1a55515996b5247dba984bc0
Andrey Popp <8mayday@gmail.com>
2023-11-15 00:31:00 +0400

    ppx_deriving_json: import as submodule

--- ppx_deriving_json/native/ppx_deriving_json_native_test.ml
+++
@@ -1,3 +0,0 @@
-open Ppxlib
-
-let () = Driver.standalone ()
--- ppx_deriving_json/test/gen_test.sh
+++
@@ -1,8 +0,0 @@
-echo "  $ alias run='$1 -impl - | ocamlformat - --impl'";
-echo "";
-cat ./example.ml | grep '^type' | while read line; do
-  echo '  $ cat <<"EOF" | run';
-  echo "  > $line";
-  echo '  > EOF';
-  echo '';
-done
