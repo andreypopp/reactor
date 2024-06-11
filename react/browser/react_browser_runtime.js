8d26fecd7bfbdec77b588099c1b89909f8a57593
Andrey Popp <8mayday@gmail.com>
2024-06-11 12:55:17 +0400

    react: proper http chunked encoding for rsc

--- react/browser/react_browser_runtime.js
+++ react/browser/react_browser_runtime.js
@@ -23,20 +23,74 @@ let loading = null;
 let loadingController = null;
 let root = null;
 
+class HTTPChunkedParser {
+  constructor() {
+    this.remainder = new Uint8Array();
+    this.textDecoder = new TextDecoder();
+  }
+
+  transform(chunk, controller) {
+    let data = new Uint8Array(this.remainder.length + chunk.length);
+    data.set(this.remainder);
+    data.set(chunk, this.remainder.length);
+
+    let offset = 0;
+    while (offset < data.length) {
+      if (offset + 2 > data.length) break; // need at least 2 bytes
+
+      // find the end of the length line (CRLF)
+      let endOfLine = data.subarray(offset).indexOf(0x0A); // 0x0A is newline in ASCII
+      if (endOfLine === -1) break; // need more data
+      let lengthS = data.subarray(offset, offset + endOfLine);
+      let length = parseInt(this.textDecoder.decode(lengthS).trim(), 16);
+
+      if (isNaN(length)) {
+        console.error("Invalid chunk length");
+        controller.error("Invalid chunk length");
+        return;
+      }
+
+      // Calculate start and end of the data chunk
+      let start = offset + endOfLine + 1;
+      let end = start + length;
+      if (end > data.length) break; // need more data
+
+      controller.enqueue(data.subarray(start, end)); // extract the data chunk and enqueue it
+      offset = end + 2; // move the offset past this chunk and the following CRLF
+    }
+    this.remainder = data.subarray(offset);
+  }
+
+  flush(controller) {
+    if (this.remainder.length > 0) {
+      // Handle any remaining data that was not a complete chunk
+      console.error("Incomplete final chunk");
+      controller.error("Incomplete final chunk");
+    }
+  }
+}
+
+function fetchRSC(path) {
+  return fetch(path, {
+    method: 'GET',
+    headers: {Accept: 'application/react.component'},
+    signal: loadingController.signal,
+  }).then(response => {
+    let p = new HTTPChunkedParser();
+    let t = new TransformStream(p);
+    response.body.pipeThrough(t);
+    response = {body: t.readable};
+    return response;
+  });
+}
+
 function loadPage(path) {
   React.startTransition(() => {
     if (loadingController != null) {
       loadingController.abort();
     }
     loadingController = new AbortController();
-    loading = ReactServerDOM.createFromFetch(
-      fetch(path, { 
-        method: "GET", 
-        headers: {Accept: 'application/react.component'},
-        signal: loadingController.signal,
-      }),
-      { callServer, }
-    );
+    loading = ReactServerDOM.createFromFetch(fetchRSC(path), { callServer });
     if (root === null)
       root = ReactDOM.createRoot(document);
     root.render(
--- react/dream/react_dream.ml
+++ react/dream/react_dream.ml
@@ -19,6 +19,28 @@ let html_prelude ~links =
 
 let rsc_content_type = "application/react.component"
 
+module Chunked = struct
+  type stream = { s : Dream.stream; is_len_encoded : bool }
+
+  let write ?(flush = false) { s; is_len_encoded } data =
+    (if is_len_encoded then
+       let len = String.length data in
+       let len = Printf.sprintf "%x\r\n" len in
+       Dream.write s len >>= fun () ->
+       Dream.write s data >>= fun () -> Dream.write s "\r\n"
+     else Dream.write s data)
+    >>= fun () -> if flush then Dream.flush s else Lwt.return_unit
+
+  let finish { s; is_len_encoded } =
+    (if is_len_encoded then Dream.write s "0\r\n\r\n" else Lwt.return ())
+    >>= fun () -> Dream.flush s
+
+  let stream ?headers ~is_len_encoded f =
+    Dream.stream ?headers (fun s ->
+        let s = { s; is_len_encoded } in
+        f s >>= fun () -> finish s)
+end
+
 let render ?(enable_client_components = false) ?(enable_ssr = true)
     ?(scripts = []) ?(links = []) =
   let html_prelude = html_prelude ~links in
@@ -30,9 +52,8 @@ let render ?(enable_client_components = false) ?(enable_ssr = true)
     | Some accept
       when enable_client_components
            && String.equal accept rsc_content_type ->
-        Dream.stream (fun stream ->
-            render_to_model ui (fun data ->
-                Dream.write stream data >>= fun () -> Dream.flush stream))
+        Chunked.stream ~is_len_encoded:true @@ fun s ->
+        render_to_model ui (Chunked.write ~flush:true s)
     | _ ->
         if enable_ssr then
           render_to_html ~render_model:enable_client_components ui
@@ -42,15 +63,14 @@ let render ?(enable_client_components = false) ?(enable_ssr = true)
                 Html.(
                   splice [ html_prelude; html; html_scripts ] |> to_string)
           | Html_rendering_async { html_shell; html_iter } ->
-              Dream.stream (fun stream ->
-                  let write_and_flush html =
-                    Dream.write stream (Html.to_string html) >>= fun () ->
-                    Dream.flush stream
-                  in
-                  write_and_flush
-                    Html.(
-                      splice [ html_prelude; html_shell; html_scripts ])
-                  >>= fun () -> html_iter write_and_flush)
+              let header =
+                Html.(splice [ html_prelude; html_shell; html_scripts ])
+              in
+              Chunked.stream ~is_len_encoded:false
+                ~headers:[ "Content-Type", "text/html" ]
+              @@ fun s ->
+              let write_html h = Chunked.write s (Html.to_string h) in
+              write_html header >>= fun () -> html_iter write_html
         else
           Dream.html
             Html.(splice [ html_prelude; html_scripts ] |> to_string)
