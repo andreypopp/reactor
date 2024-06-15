0cadad4901c23d616004808112c24765290507a2
Andrey Popp <8mayday@gmail.com>
2024-06-15 22:24:03 +0400

    example/rsc: show case how to load data on client

--- example/rsc/routing/routing.ml
+++ example/rsc/routing/routing.ml
@@ -1,4 +1,7 @@
+open Ppx_deriving_json_runtime.Primitives
+
 type _ t =
   | Home : Ppx_deriving_router_runtime.response t [@GET "/"]
   | About : Ppx_deriving_router_runtime.response t [@GET "/about"]
+  | Api_server_time : float t [@GET "/api/server-time"]
 [@@deriving router]
